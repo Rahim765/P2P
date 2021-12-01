0d4d6260b58e8caaceacdfa98498f310dd673a0f
sz <shoayb.zaynali@yahoo.com>
2021-12-01 16:32:57 +0330

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        //sz is testing some shit
      //   ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
         File file = new File("Config.yml");
